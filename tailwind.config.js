24bb36855f7dd0e6dad4db0210e83fe31c96da63
Eve-code93 <evelynejose1993@gmail.com>
2025-04-11 10:49:24 +0300

    Set up Tailwind CSS with Vite and initial styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+// tailwind.config.js
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your file types
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
